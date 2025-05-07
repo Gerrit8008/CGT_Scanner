3bc7e545db1c3454fb63b3763832558b63b12c8c
Gerrit8008 <gerrit.grundling@gmail.com>
2025-05-07 15:57:50 -0400

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-# For debugging, import from app_debug instead of app
-try:
-    from app_debug import app
-except ImportError:
-    # Fall back to regular app if debug version is not available
-    from app import app
-
-if __name__ == "__main__":
-    app.run()
