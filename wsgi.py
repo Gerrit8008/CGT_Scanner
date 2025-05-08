16d987433e130171e8ac34e7fd285d2a2187cc1e
Gerrit8008 <gerrit.grundling@gmail.com>
2025-05-08 15:19:55 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,8 +3,11 @@ import logging
 from app import app
 
 # Set up logging configuration
-logging.basicConfig(level=logging.DEBUG, 
+logging.basicConfig(level=logging.INFO, 
                     format='%(asctime)s - %(levelname)s - %(message)s')
+
+# This is the application object that will be used by the WSGI server
+application = app
 
 if __name__ == "__main__":
     # Get port from environment variable or use default
